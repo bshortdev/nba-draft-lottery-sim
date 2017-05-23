f3396734f184d1598e5d79a34d5e2f7087fd3be9
Brandon <bshortdev@gmail.com>
2017-05-23 19:00:25 -0400

    Created the Team class

---
+++ NBADraftLotterySim/Team.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBADraftLotterySim
+{
+    class Team
+    {
+        private string teamName { get; set; }
+        private int lotteryOdds { get; set; }
+
+        public Team(string name, int odds)
+        {
+            teamName = name;
+            lotteryOdds = odds;
+        }
+    }
+}
