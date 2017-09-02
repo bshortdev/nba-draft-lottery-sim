efa66e319bf38033fa5165ff0e3947acf5759680
Brandon <bshortdev@gmail.com>
2017-09-02 00:44:33 -0400

    Added 2014, 2016, & 2017 Lottery data

--- NBADraftLotterySim/Years.cs
+++ NBADraftLotterySim/Years.cs
@@ -9,7 +9,39 @@ namespace NBADraftLotterySim
     class Years
     {
         public Team[] chosenLotto { get; set; }
-        private Team[] lottery2015 = new Team[] { new Team("Minnesota Timberwolves", 250, 1),
+        private Team[] lottery2014 = new Team[] {
+            new Team("Milwaukee Bucks", 250, 1),
+                new Team("Philadephia 76ers", 199, 2),
+                new Team("Orlando Magic", 156, 3),
+                new Team("Utah Jazz", 119, 4),
+                new Team("Boston Celtics", 88, 5),
+                new Team("Los Angeles Lakers", 63, 6),
+                new Team("Sacramento Kings", 43, 7),
+                new Team("Detroit Pistons", 28, 8),
+                new Team("Cleveland Cavaliers", 17, 9),
+                new Team("New Orleans Pelicans", 11, 10),
+                new Team("Denver Nuggets", 8, 11),
+                new Team("New York Knicks", 7, 12),
+                new Team("Minnesota Timberwolves", 6, 13),
+                new Team("Phoenix Suns", 5, 14) };
+
+        private static Team[] actual2014 = new Team[] {
+            new Team("Cleveland Cavaliers"),
+                new Team("Milwaukee Bucks"),
+                new Team("Philadelphia 76ers"),
+                new Team("Orlando Magic"),
+                new Team("Utah Jazz"),
+                new Team("Boston Celtics"),
+                new Team("Los Angeles Lakers"),
+                new Team("Sacramento Kings"),
+                new Team("Charlotte Hornets"),
+                new Team("Philadelphia 76ers"),
+                new Team("Denver Nuggets"),
+                new Team("Orlando Magic"),
+                new Team("Minnesota Timberwolves"),
+                new Team("Phoenix Suns")};
+        private Team[] lottery2015 = new Team[] {
+            new Team("Minnesota Timberwolves", 250, 1),
                 new Team("New York Knicks", 199, 2),
                 new Team("Philadelphia 76ers", 156, 3),
                 new Team("Los Angeles Lakers", 119, 4),
@@ -24,7 +56,8 @@ namespace NBADraftLotterySim
                 new Team("Phoenix Suns", 6, 13),
                 new Team("Oklahoma City Thunder", 5, 14) };
 
-        private static Team[] actual2015 = new Team[] { new Team("Minnesota Timberwolves"),
+        private static Team[] actual2015 = new Team[] {
+            new Team("Minnesota Timberwolves"),
                 new Team("Los Angeles Lakers"),
                 new Team("Philadelphia 76ers"),
                 new Team("New York Knicks"),
@@ -39,11 +72,83 @@ namespace NBADraftLotterySim
                 new Team("Phoenix Suns"),
                 new Team("Oklahoma City Thunder")};
 
+        private Team[] lottery2016 = new Team[] {
+            new Team("Philadelphia 76ers", 250, 1),
+                new Team("Los Angeles Lakers", 199, 2),
+                new Team("Boston Celtics", 156, 3),
+                new Team("Phoenix Suns", 119, 4),
+                new Team("Minnesota Timberwolves", 88, 5),
+                new Team("New Orleans Pelicans", 63, 6),
+                new Team("New York Knicks", 43, 7),
+                new Team("Sacramento Kings", 19, 8),
+                new Team("Denver Nuggets", 19, 9),
+                new Team("Milwaukee Bucks", 18, 10),
+                new Team("Orlando Magic", 8, 11),
+                new Team("Utah Jazz", 7, 12),
+                new Team("Washington Wizards", 6, 13),
+                new Team("Chicago Bulls", 5, 14) };
+
+        private static Team[] actual2016 = new Team[] {
+            new Team("Philadelphia 76ers"),
+                new Team("Los Angeles Lakers"),
+                new Team("Boston Celtics"),
+                new Team("Phoenix Suns"),
+                new Team("Minnesota Timberwolves"),
+                new Team("New Orleans Pelicans"),
+                new Team("Denver Nuggets"),
+                new Team("Sacramento Kings"),
+                new Team("Toronto Raptors"),
+                new Team("Milwaukee Bucks"),
+                new Team("Orlando Magic"),
+                new Team("Utah Jazz"),
+                new Team("Phoenix Suns"),
+                new Team("Chicago Bulls") };
+
+        private Team[] lottery2017 = new Team[] {
+            new Team("Boston Celtics", 250, 1),
+                new Team("Phoenix Suns", 199, 2),
+                new Team("Los Angeles Lakers", 156, 3),
+                new Team("Philadelphia 76ers", 119, 4),
+                new Team("Orlando Magic", 88, 5),
+                new Team("Minnesota Timberwolves", 53, 6),
+                new Team("New York Knicks", 53, 7),
+                new Team("Sacramento Kings", 28, 8),
+                new Team("Dallas Mavericks", 17, 9),
+                new Team("New Orleans Pelicans", 11, 10),
+                new Team("Charlotte Hornets", 8, 11),
+                new Team("Detroit Pistons", 7, 12),
+                new Team("Denver Nuggets", 6, 13),
+                new Team("Miami Heat", 5, 14) };
+
+        private static Team[] actual2017 = new Team[] { 
+            new Team("Boston Celtics"),
+                new Team("Los Angeles Lakers"),
+                new Team("Sacramento Kings"),
+                new Team("Phoenix Suns"),
+                new Team("Philadelphia 76ers"),
+                new Team("Orlando Magic"),
+                new Team("Minnesota Timberwolves"),
+                new Team("New York Knicks"),
+                new Team("Dallas Mavericks"),
+                new Team("New Orleans Hornets"),
+                new Team("Charlotte Hornets"),
+                new Team("Detroit Pistons"),
+                new Team("Denver Nuggets"),
+                new Team("Miami Heat")};
+
         public Years(int year)
         {
-            if(year == 2015)
+            switch(year)
             {
-                setTeams(lottery2015);
+                case 2015:
+                    setTeams(lottery2015);
+                    break;
+                case 2016:
+                    setTeams(lottery2016);
+                    break;
+                case 2017:
+                    setTeams(lottery2017);
+                    break;
             }
         }
 
@@ -54,14 +159,20 @@ namespace NBADraftLotterySim
 
         public static Lottery getTrueLottery(int year)
         {
-            if(year == 2015)
+            Lottery ret = null;
+            switch (year)
             {
-                return new Lottery(actual2015);
+                case 2015:
+                    ret = new Lottery(actual2015);
+                    break;
+                case 2016:
+                    ret = new Lottery(actual2016);
+                    break;
+                case 2017:
+                    ret = new Lottery(actual2017);
+                    break;
             }
-            else
-            {
-                return null;
-            }
+            return ret;
         }
     }
 }
